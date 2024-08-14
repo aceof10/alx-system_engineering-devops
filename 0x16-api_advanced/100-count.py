17446085e87b5dd7545b6d6e9071d99b8448fca2
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-08-14 11:11:38 +0300

    reorganize imports to be alphabetically sorted

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,8 +4,8 @@ queries the Reddit API, parses the title of all hot articles, and prints a
 sorted count of given keywords (case-insensitive, delimited by spaces.
 Javascript should count as javascript, but java should not)
 """
-import requests
 from collections import Counter
+import requests
 
 
 def count_words(subreddit, word_list, hot_list=[], after=None):
